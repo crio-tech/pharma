ba9305cde102a70cd7fcbe077be7d568ac587da5
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2020-08-27 00:11:18 +0530

    initial doctype creation

---
+++ pharmacy/pharmacy/doctype/cheque/cheque.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, crio and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Cheque', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ pharmacy/pharmacy/doctype/pharma/pharma.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, crio and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Pharma', {
+	// refresh: function(frm) {
+
+	// }
+});
