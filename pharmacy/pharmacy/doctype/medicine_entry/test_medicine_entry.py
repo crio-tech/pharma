9bac6d531139b4519526f6371d526d8a570a7130
Mohammed Haris <mohammedharis.k.01@gmail.com>
2021-03-19 23:12:58 +0530

    Customise doctype

---
+++ pharmacy/pharmacy/doctype/medicine_entry/test_medicine_entry.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, crio and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestMedicineEntry(unittest.TestCase):
+	pass
